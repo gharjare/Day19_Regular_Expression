a09370f68e95b004e545d82ec048cf05f3ca1086
gharjare <gharjarechetan@gmail.com>
2022-12-20 12:46:31 +0530

    add one upper case

---
+++ UpperCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public class UpperCase
+    {
+        public static string Regex_Upper = "((?=.*[A-Z]).{8,})";
+        public bool ValidateUpper(string Upper)
+        {
+            return Regex.IsMatch(Upper, Regex_Upper);
+        }
+    }
+}
