a6b908651136ec217dd8e13475322b5c566448ef
gharjare <gharjarechetan@gmail.com>
2022-12-20 12:48:15 +0530

    add one number in password

---
+++ NumberInPassword.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public class NumberInPassword
+    {
+        public static string Regex_Numeric = "((?=.*[A-Z])(?=.*[0-9]).{8,})";
+        public bool ValidateNumeric(string Numeric)
+        {
+            return Regex.IsMatch(Numeric, Regex_Numeric);
+        }
+    }
+}
