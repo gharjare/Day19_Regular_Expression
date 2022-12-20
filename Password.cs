eb56570c581a27bab54552a77ec9dd81409eb1fd
gharjare <gharjarechetan@gmail.com>
2022-12-20 12:45:13 +0530

    add valid password

---
+++ Password.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public class Password
+    {
+        public static string Regex_Password = "^[A-Za-z0-9]{8}$";
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
